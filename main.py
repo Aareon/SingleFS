2d486e778fd123d5bd133b6db2fb3beb5dcfe890
Aareon <askully13@gmail.com>
2023-04-15 15:37:46 -0500

    try to get uploading working (broken)

--- main.py
+++ main.py
@@ -5,11 +5,18 @@ from pathlib import Path
 import os
 import string
 import random
+import uuid
+
+ROOT_PATH = Path(__file__).parent
+
+UPLOAD_FOLDER = ROOT_PATH / "uploads"
+UPLOAD_FOLDER.mkdir(exist_ok=True)
 
 app = Flask(__name__)
 DB = Database()
 app.config['SESSION_TYPE'] = 'filesystem'
 app.config["SECRET_KEY"] = os.urandom(24)
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 
 @app.route('/')
@@ -53,13 +60,22 @@ def logout():
     return redirect(url_for("index"))
 
 
+@app.route('/upload', methods=['POST'])
+def upload():
+    file = request.files['file']
+    filename = f"{uuid.uuid4()}.jpg"  # use UUID to generate a unique filename
+    filepath = app.config['UPLOAD_FOLDER'].joinpath(filename)
+    file.save(filepath)
+    return '', 204  # empty response with status code 204 to indicate success
+
+
 def gen_access_token():
     chars = string.ascii_uppercase + string.digits
     return "".join(random.choice(chars) for _ in range(12))
 
 
 if __name__ == "__main__":
-    if not os.path.exists("access_token.txt"):
+    if not (ROOT_PATH / "access_token.txt").exists():
         with open("access_token.txt", "w") as f:
             f.write(gen_access_token())
     
